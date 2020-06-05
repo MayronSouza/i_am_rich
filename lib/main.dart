df3950a25a4b84a463012e00688d307be5e62db3
MayronGMZ <mayron.souza@gmz.solutions>
2020-06-05 19:05:13 -0300

    Incluindo imagens por URL

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,16 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          backgroundColor: Colors.brown[100],
           appBar: AppBar(
+            centerTitle: true,
             title: Text('I am Rich'),
             backgroundColor: Colors.brown,
+          ),
+          body: Center(
+            child: Image(
+            image: NetworkImage('https://www.gettyimages.com.br/gi-resources/images/500px/983794168.jpg'),
+          ),
           ),
         ),
       ),
